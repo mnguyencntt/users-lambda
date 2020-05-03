bffae9d93520e6ee4d6a193189de9f9d83a965d3
MinhNguyen <MinhNguyen>
2020-05-03 22:56:32 +0800

    add jacoco and @Disable

--- src/test/java/com/anz/platform/service/UserFunctionTest.java
+++ src/test/java/com/anz/platform/service/UserFunctionTest.java
@@ -1,5 +1,6 @@
 package com.anz.platform.service;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import com.anz.platform.base.BaseTest;
 import com.anz.platform.config.AppConfig;
@@ -9,6 +10,7 @@ public class UserFunctionTest extends BaseTest {
   private AppConfigMock appConfigMock = new AppConfigMock();
 
   @SuppressWarnings("unused")
+  @Disabled
   @Test
   public void testUser() {
     UserFunction userFunction = new UserFunction(appConfigMock.getDbInfo());
